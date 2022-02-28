5dc7de4e5b6428e68f4909d61b159a8b102ffa2f
Clovis Inaka <clovadevjob>
2022-02-27 18:44:37 -0600

    First revert commit lesson

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import com.example.calculator.databinding.ActivityMainBinding
 
+//clovis
+
 class MainActivity : AppCompatActivity() {
 
     private val binding: ActivityMainBinding by lazy {
